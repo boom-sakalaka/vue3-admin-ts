b97397556e2819ae3cadff7d79100182bc07f506
gzh <936285702@qq.com>
2021-12-31 17:56:42 +0800

    axios Promise 封装

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,28 +2,34 @@
  * @Author: GZH
  * @Date: 2021-12-29 16:25:44
  * @LastEditors: GZH
- * @LastEditTime: 2021-12-29 16:43:20
+ * @LastEditTime: 2021-12-29 17:07:45
  * @FilePath: \qrcode-proj\src\store\getters.ts
  * @Description:
  */
 import { genenrateColor } from '@/utils/theme'
 import { getItem } from '@/utils/storage'
 import { MAIN_COLOR } from '@/constant'
+import { IUser } from './modules/user'
+
+// 这样子的类型定义好像没什么意义，后续跟进去更改，或许会换成 Pinia ?
+export interface IAllState {
+  user: IUser
+}
 
 // console.warn(genenrateColor(getItem(MAIN_COLOR)))
 const getters = {
-  token: (state: any) => state.user.token,
-  hasUserInfo: (state: any) => {
+  token: (state: IAllState) => state.user.token,
+  hasUserInfo: (state: IAllState) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  userInfo: (state: any) => state.user.userInfo,
-  cssVar: (state: any) => ({
-    ...state.theme.variables,
-    ...genenrateColor(getItem(MAIN_COLOR) as string)
-  }),
-  sidebarOpened: (state: any) => state.app.sidebarOpened,
-  language: (state: any) => state.app.language,
-  mainColor: (state: any) => state.theme.mainColor,
-  tagsViewList: (state: any) => state.app.tagsViewList
+  userInfo: (state: IAllState) => state.user.userInfo
+  // cssVar: (state: IAllState) => ({
+  //   ...state.theme.variables,
+  //   ...genenrateColor(getItem(MAIN_COLOR) as string)
+  // }),
+  // sidebarOpened: (state: IAllState) => state.app.sidebarOpened,
+  // language: (state: IAllState) => state.app.language,
+  // mainColor: (state: IAllState) => state.theme.mainColor,
+  // tagsViewList: (state: IAllState) => state.app.tagsViewList
 }
 export default getters
---
+++ src/utils/request2.ts
@@ -0,0 +1,110 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-12-29 13:53:43
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-12-31 17:56:29
+ * @FilePath: \qrcode-proj\src\utils\request2.ts
+ * @Description:封装登录请求 https://github.dev/buqiyuan/vue3-antd-admin
+ */
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { ElMessage } from 'element-plus'
+import store from '@/store'
+import { isCheckTimeout } from '@/utils/auth'
+
+export interface RequestOptions {
+  /*  当前接口权限，不需要接口鉴权可忽略 */
+  permCode?: string
+  /*  是否直接获取data，忽略message */
+  isGetDataDirectly?: boolean
+  /* 请求成功是否提示 */
+  successMsg?: string
+  /* 请求失败是否提示 */
+  errorMsg?: string
+  /* 是否mock请求 */
+  isMock?: boolean
+}
+
+const UNKNOWN_ERROR = '未知错误，请重试'
+
+/* 真实请求路径前缀 */
+const baseUrl = process.env.VUE_APP_BASE_API as string
+/* mock请求路径前缀 */
+const mockUrl = process.env.VUE_APP_MOCK_API as string
+
+const service = axios.create({
+  // baseURL:
+  timeout: 50000
+})
+
+service.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token && config.headers) {
+      config.headers.Authorization = token
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data
+    if (res.code !== 200) {
+      ElMessage.error(res.message || UNKNOWN_ERROR)
+
+      const error = new Error(res.message || UNKNOWN_ERROR) as Error & { code: any }
+      error.code = res.code
+      return Promise.reject(error)
+    } else {
+      return res
+    }
+  },
+  (error) => {
+    // 处理 422 或者 500 的错误异常提示
+    const errMsg = error?.response?.data?.message ?? UNKNOWN_ERROR
+    ElMessage.error(errMsg)
+    error.message = errMsg
+    return Promise.reject(error)
+  }
+)
+
+export type Response<T = any> = {
+  code: number
+  message: string
+  data: T
+}
+
+export type BaseResponse<T = any> = Promise<Response<T>>
+
+/**
+ *
+ * @param method - request methods
+ * @param url - request url
+ * @param data - request data or params
+ */
+export const request = async <T = any>(
+  config: AxiosRequestConfig,
+  options: RequestOptions = {}
+): Promise<T> => {
+  try {
+    const { successMsg, errorMsg, permCode, isMock, isGetDataDirectly = true } = options
+    // 如果当前是需要鉴权的接口 并且没有权限的话 则终止请求发起
+    // !useUserStore().perms.includes(permCode)
+    if (permCode) {
+      ElMessage.error('你没有访问该接口的权限，请联系管理员！')
+      return Promise.reject(new Error('e'))
+    }
+    const fullUrl = `${(isMock ? mockUrl : baseUrl) + config.url}`
+    config.url = fullUrl.replace(/(?<!:)\/{2,}/g, '/')
+
+    const res = await service.request(config)
+    successMsg && ElMessage.success(successMsg)
+    errorMsg && ElMessage.error(errorMsg)
+    return isGetDataDirectly ? res.data : res
+  } catch (error: any) {
+    return Promise.reject(error)
+  }
+}
